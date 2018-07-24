5c4791c9e814fa714878c27c7be40321ac6cff56
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-07-24 10:55:38 -0700

    temp solution

--- install_cuda.sh
+++ install_cuda.sh
@@ -11,7 +11,7 @@ export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
 
 sudo apt-get update
-sudo apt-get install -y cuda
+sudo apt-get install -y --allow-unauthenticated cuda
 
 # Check if installation is successful by running the next line
 # nvcc -V
