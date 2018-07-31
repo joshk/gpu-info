c16b102ca6aa6f87920d77bf7eb03dec59b5dde6
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-07-30 17:44:33 -0700

    following some german google instructions
    https://cloud.google.com/compute/docs/gpus/add-gpus?hl=de#install-driver-manual

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,11 +1,11 @@
 set -ex
 
 # http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/
-curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
 
-sudo dpkg -i --debug=210 cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
 
-rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
+rm cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
 
 export CUDA_HOME=/usr/local/cuda
 export CUDA_ROOT=/usr/local/cuda
@@ -13,8 +13,10 @@ export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
 
 sudo apt-get update -y --allow-unauthenticated
-sudo apt-get install -y --allow-unauthenticated cuda
+sudo apt-get install -y --allow-unauthenticated cuda-8-0
 
-sudo nvidia-smi
+sudo apt-get install linux-headers-$(uname -r)
+
+sudo nvidia-smi -pm 1
 # Check if installation is successful by running the next line
 # nvcc -V
